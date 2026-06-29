42ce6e9ef9c84c04f00fdc1d857997dd42a21a36
skirlez <skirlez3@gmail.com>
2026-06-29 21:54:59 +0300

    change how movement works

--- player.gd
+++ player.gd
@@ -9,10 +9,25 @@ enum States {
 
 var timer: int = 0
 var state = States.WAITING
-var walk_angle: float = 0
 
-var walk_start
-var walk_destination
+
+enum WalkAngle {
+	RIGHT = 0,
+	UP = 1,
+	LEFT = 2,
+	DOWN = 3
+}
+const WALK_ANGLE_OFFSETS = [Vector2i(1, 0), Vector2i(0, -1), Vector2i(-1, 0), Vector2i(0, 1)]
+var walk_angle = WalkAngle.UP
+
+const PERM_Y_OFFSET = 1;
+const JUMP_VELOCITY = 0.3;
+var y_offset = 0;
+var y_speed = 0;
+
+var pos: Vector2i
+var walk_start: Vector2i
+var walk_destination: Vector2i
 
 var rotation_direction: int
 var rotation_start: int
@@ -28,79 +43,99 @@ const RECT_SIZE = 4
 var total_walk_tics = 20
 var total_rotation_tics = 20
 
+func grid_to_world_2d(grid_pos: Vector2i) -> Vector2:
+	const GRID_OFFSET = Vector2i.ONE * PATTERN_SIZE
+	const WORLD_OFFSET = Vector2.ONE * 0.5
+	return (Vector2(grid_pos - GRID_OFFSET) - WORLD_OFFSET) * RECT_SIZE
+func grid_to_world_3d(grid_pos: Vector2i) -> Vector3:
+	return world_2d_to_world_3d(grid_to_world_2d(grid_pos))
+func world_2d_to_world_3d(world_pos: Vector2) -> Vector3:
+	return Vector3(world_pos.x, PERM_Y_OFFSET, world_pos.y)
 
 func _ready():
-	transform.origin.x = RECT_SIZE * 12.5
-	transform.origin.z = RECT_SIZE * 0.5
+	pos.x = 28
+	pos.y = 16
+	update_origin(pos)
+	update_basis(walk_angle)
+	
 func switch_states(new):
 	timer = 0
 	state = new
 	state_epoch = Time.get_ticks_msec()
 	match state:
 		States.WALKING:
-			walk_start = transform.origin
-			walk_destination = transform.origin + RECT_SIZE * Vector3.FORWARD.rotated(Vector3.DOWN, deg_to_rad(walk_angle)) 
-		States.ROTATING:
-			rotation_start = int(walk_angle)
-			rotation_destination = (rotation_start + 90 * rotation_direction)
-func update_basis(angle):
-	transform.basis = Basis.from_euler(Vector3(0, -deg_to_rad(angle), 0)) 
+			walk_start = Vector2(pos.x, pos.y)
 			
+			@warning_ignore("integer_division")			
+			var dest_offset = WALK_ANGLE_OFFSETS[walk_angle]
+			walk_destination = walk_start + dest_offset
+		States.ROTATING:
+			rotation_start = walk_angle
+			rotation_destination = rotation_start + rotation_direction
+	
+func update_basis(angle):
+	transform.basis = Basis.from_euler(Vector3(0, (angle * PI / 2) - PI / 2, 0)) 
+func update_origin(new_pos):
+	transform.origin = grid_to_world_3d(new_pos)
+	
 func _process(_delta: float) -> void:	
 	RenderingServer.global_shader_parameter_set("player_pos", transform.origin)
 	var time = (Time.get_ticks_msec() - state_epoch)
 	var frame_times = time * 60.0 / 1000.0
 	match state:
 		States.WALKING:
-			transform.origin = lerp(walk_start, walk_destination, min(1, frame_times / float(total_walk_tics)))
+			var new_pos = lerp(grid_to_world_2d(walk_start), grid_to_world_2d(walk_destination), min(1, frame_times / float(total_walk_tics)))
+			transform.origin = world_2d_to_world_3d(new_pos) 
 		States.ROTATING:
-			var angle = lerp(rotation_start, rotation_destination, min(1, frame_times / float(total_rotation_tics)))
+			var angle = lerp(float(rotation_start), float(rotation_destination), min(1, frame_times / float(total_rotation_tics)))
 			update_basis(angle)
 
 
 func _physics_process(_delta: float) -> void:
-
-	# kinda fighting with the engine here to detect collisions but i don't think area3d has any deterministic guarantees or whatever
-
-	var s = RECT_SIZE / 2.0
-	# top left most tile
-	var top_left = Vector3(-s - PATTERN_SIZE * RECT_SIZE, 0, s - PATTERN_SIZE * RECT_SIZE)
-
-	
+	y_offset += y_speed
+	if y_offset > 0:
+		y_speed -= 0.01
+	else:
+		y_offset = 0
+	$Mesh.transform.origin.y = y_offset
 	timer += 1
 	match state:
 		States.WAITING:
 			if (timer == 180):
 				switch_states(States.WALKING)
 		States.WALKING:
-			var interm = lerp(walk_start, walk_destination, min(1, timer / float(total_walk_tics)))
-			var my_tile = interm - top_left
-			
-			my_tile.x = round(my_tile.x / RECT_SIZE)
-			my_tile.z = round(my_tile.z / RECT_SIZE) + 1
-			var index = my_tile.z * LEVEL_SIZE + my_tile.x
-			var instances =	$"../LevelObjects".instances
-			if index >= 0 and index <= len(instances) - 1:
-				var inst = instances[index]
-				if is_instance_valid(inst):
-					on_collide_with_instance(inst)
-			
-			
+			if Input.is_action_just_pressed("jump"):
+				if y_offset == 0:
+					y_speed = JUMP_VELOCITY 
+					$Jump.play()
 			if (timer == total_walk_tics):
-				transform.origin = walk_destination
-				if Input.is_action_pressed("rotate_left"):
-					rotation_direction = -1
+				if y_offset < 0.5:
+					var my_tile = walk_destination
+					var index = my_tile.y * LEVEL_SIZE + my_tile.x
+					var instances =	$"../LevelObjects".instances
+					if index >= 0 and index <= len(instances) - 1:
+						var inst = instances[index]
+						if is_instance_valid(inst):
+							on_collide_with_instance(inst)
+
+				pos = walk_destination
+				transform.origin = grid_to_world_3d(pos)
+				if y_offset != 0:
+					switch_states(States.WALKING)
+				elif Input.is_action_pressed("rotate_left"):
+					rotation_direction = 1
 					switch_states(States.ROTATING)
 				elif Input.is_action_pressed("rotate_right"):
-					rotation_direction = 1
+					rotation_direction = -1
 					switch_states(States.ROTATING)
 				else:
 					switch_states(States.WALKING)
 		States.ROTATING:
 			if (timer == total_rotation_tics):
-				walk_angle = rotation_destination % 360
-				if walk_angle < 0:
-					walk_angle += 360
+				var angle_final = rotation_destination % 4
+				if angle_final < 0:
+					angle_final += 4
+				walk_angle = angle_final as WalkAngle 
 				update_basis(walk_angle)
 				switch_states(States.WALKING)
 
